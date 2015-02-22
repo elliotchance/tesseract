b630211bfcc052da4f6b32585433b71222c3453f
Elliot Chance <elliotchance@gmail.com>
2015-02-22 18:06:13 +1100

    test_providing_a_server_that_doesnt_exist

--- tesseract/server.py
+++ tesseract/server.py
@@ -1,13 +1,23 @@
 import tesseract.sql.parser as parser
 from tesseract.sql.objects import *
+import redis
 
 class Server:
     """
     A server will execute SQL commands and return their result.
     """
 
-    def __init__(self):
-        self.records = []
+    def __init__(self, redis_host=None):
+        # The default Redis host is `localhost` if it is not provided
+        if not redis_host:
+            redis_host = 'localhost'
+
+        # The Redis host must be a `str`.
+        assert isinstance(redis_host, str)
+
+        # Attempt to connect.
+        self.redis = redis.StrictRedis(host=redis_host, port=6379, db=0)
+        self.redis.set('tesseract_server', 1)
 
     def execute(self, sql):
         """
@@ -28,11 +38,14 @@ class Server:
 
         # If the statement is an `INSERT` we always return success.
         if isinstance(result.statement, InsertStatement):
-            self.records.append(result.statement.fields)
+            self.redis.lpush(result.statement.table_name,
+                             json.dumps(result.statement.fields))
             return ServerResult(True)
 
         # This is a `SELECT`
-        return ServerResult(True, self.records)
+        page = self.redis.lrange(result.statement.table_name, 0, -1)
+        all_records = [json.loads(object) for object in page]
+        return ServerResult(True, all_records)
 
 
 class ServerResult:
--- tesseract/test_server.py
+++ tesseract/test_server.py
@@ -10,6 +10,7 @@ class TestServer(TestCase):
 
     def test_select_from_table_that_doesnt_exist(self):
         server = Server()
+        server.redis.delete('foo')
         result = server.execute('SELECT * FROM foo')
         self.assertTrue(result.success)
         self.assertEqual(result.data, [])
@@ -22,6 +23,7 @@ class TestServer(TestCase):
 
     def test_insert_and_select(self):
         server = Server()
+        server.redis.delete('foo')
         server.execute('INSERT INTO foo {"foo": "bar"}')
         result = server.execute('SELECT * FROM foo')
         self.assertTrue(result.success)
@@ -31,11 +33,20 @@ class TestServer(TestCase):
 
     def test_insert_multiple_and_select(self):
         server = Server()
+        server.redis.delete('foo')
         server.execute('INSERT INTO foo {"foo": "bar"}')
         server.execute('INSERT INTO foo {"bar": "baz"}')
         result = server.execute('SELECT * FROM foo')
         self.assertTrue(result.success)
         self.assertEqual(result.data, [
-            {"foo": "bar"},
             {"bar": "baz"},
+            {"foo": "bar"},
         ])
+
+    def test_providing_a_server_that_doesnt_exist(self):
+        try:
+            Server('nowhere')
+            self.fail("Expected failure")
+        except Exception as e:
+            self.assertEqual('Error 8 connecting to nowhere:6379. nodename nor '
+            'servname provided, or not known.', str(e))
