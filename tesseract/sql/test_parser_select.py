a38672bb58d8e8939c39918ce779bc7c61046702
Elliot Chance <elliotchance@gmail.com>
2015-02-23 21:42:16 +1100

    test_select_where_plus

--- tesseract/sql/test_parser_select.py
+++ tesseract/sql/test_parser_select.py
@@ -64,3 +64,7 @@ class TestServer(ParserTestCase):
     def test_select_where_or(self):
         result = parser.parse('SELECT * FROM foo WHERE a < 7 OR b > 15')
         self.assertEquals(str(result.statement.where), 'a < 7 OR b > 15')
+
+    def test_select_where_plus(self):
+        result = parser.parse('SELECT * FROM foo WHERE a + b')
+        self.assertEquals(str(result.statement.where), 'a + b')
