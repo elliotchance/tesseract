68172d6f0d97d88f7453fd020842b009c5ae1fd5
Elliot Chance <elliotchance@gmail.com>
2015-02-24 01:55:16 +1100

    test_nested

--- tesseract/sql/test_parser_expressions.py
+++ tesseract/sql/test_parser_expressions.py
@@ -5,50 +5,41 @@ class TestParserExpressions(ParserTestCase):
     def test_equal(self):
         self.assertSQL('a = 1', 'a = 1')
 
-    def test_select_where_not_equal(self):
-        result = parser.parse('SELECT * FROM foo WHERE a <> 2')
-        self.assertEquals(str(result.statement.where), 'a <> 2')
+    def test_not_equal_1(self):
+        self.assertSQL('a <> 2', 'a <> 2')
 
-    def test_select_where_not_equal2(self):
-        result = parser.parse('SELECT * FROM foo WHERE a != 3')
-        self.assertEquals(str(result.statement.where), 'a <> 3')
+    def test_not_equal_2(self):
+        self.assertSQL('a != 3', 'a <> 3')
 
-    def test_select_where_greater(self):
-        result = parser.parse('SELECT * FROM foo WHERE a > 4')
-        self.assertEquals(str(result.statement.where), 'a > 4')
+    def test_greater(self):
+        self.assertSQL('a > 4', 'a > 4')
 
-    def test_select_where_less(self):
-        result = parser.parse('SELECT * FROM foo WHERE a < 5')
-        self.assertEquals(str(result.statement.where), 'a < 5')
+    def test_less(self):
+        self.assertSQL('a < 5', 'a < 5')
 
-    def test_select_where_greater_equal(self):
-        result = parser.parse('SELECT * FROM foo WHERE a >= 6')
-        self.assertEquals(str(result.statement.where), 'a >= 6')
+    def test_greater_equal(self):
+        self.assertSQL('a >= 6', 'a >= 6')
 
-    def test_select_where_less_equal(self):
-        result = parser.parse('SELECT * FROM foo WHERE a <= 7')
-        self.assertEquals(str(result.statement.where), 'a <= 7')
+    def test_less_equal(self):
+        self.assertSQL('a <= 7', 'a <= 7')
 
-    def test_select_where_and(self):
-        result = parser.parse('SELECT * FROM foo WHERE a < 7 AND b > 15')
-        self.assertEquals(str(result.statement.where), 'a < 7 AND b > 15')
+    def test_and(self):
+        self.assertSQL('a AND b', 'a AND b')
 
-    def test_select_where_or(self):
-        result = parser.parse('SELECT * FROM foo WHERE a < 7 OR b > 15')
-        self.assertEquals(str(result.statement.where), 'a < 7 OR b > 15')
+    def test_or(self):
+        self.assertSQL('a OR b', 'a OR b')
 
-    def test_select_where_plus(self):
-        result = parser.parse('SELECT * FROM foo WHERE a + b')
-        self.assertEquals(str(result.statement.where), 'a + b')
+    def test_plus(self):
+        self.assertSQL('a + 3', 'a + 3')
 
-    def test_select_where_minus(self):
-        result = parser.parse('SELECT * FROM foo WHERE a - b')
-        self.assertEquals(str(result.statement.where), 'a - b')
+    def test_minus(self):
+        self.assertSQL('a - 3', 'a - 3')
 
-    def test_select_where_times(self):
-        result = parser.parse('SELECT * FROM foo WHERE a * b')
-        self.assertEquals(str(result.statement.where), 'a * b')
+    def test_times(self):
+        self.assertSQL('a * 3', 'a * 3')
 
-    def test_select_where_divide(self):
-        result = parser.parse('SELECT * FROM foo WHERE a / b')
-        self.assertEquals(str(result.statement.where), 'a / b')
+    def test_divide(self):
+        self.assertSQL('a / 3', 'a / 3')
+
+    def test_nested(self):
+        self.assertSQL('a < 7 AND b > 15', 'a < 7 AND b > 15')
