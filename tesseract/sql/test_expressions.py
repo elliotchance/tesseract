b23ac0c40332fec9e591772c62b7b232262cc94b
Elliot Chance <elliotchance@gmail.com>
2015-02-25 20:20:04 +1100

    test_integer_float

--- tesseract/sql/test_expressions.py
+++ tesseract/sql/test_expressions.py
@@ -2,6 +2,10 @@ from unittest import TestCase
 from tesseract.sql.expressions import DivideExpression
 
 class TestDivideExpression(TestCase):
-    def test_divide_integers(self):
+    def test_integer_integer(self):
         expr = DivideExpression(12, 6)
+        self.assertEqual(2, expr.eval())
+
+    def test_integer_float(self):
+        expr = DivideExpression(5, 2.5)
         self.assertEqual(2, expr.eval())
