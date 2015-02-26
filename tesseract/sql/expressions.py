4c307a612ea74f1ca63bf6e79c9982778ee76e65
Elliot Chance <elliotchance@gmail.com>
2015-02-26 23:53:05 +1100

    TestSubtractExpression

--- tesseract/sql/expressions.py
+++ tesseract/sql/expressions.py
@@ -187,6 +187,17 @@ class SubtractExpression(BinaryExpression):
     def __init__(self, left, right):
         BinaryExpression.__init__(self, left, '-', right)
 
+    def eval(self):
+        numeric = (Value.INTEGER, Value.FLOAT)
+
+        if self.left.type() in numeric and self.right.type() in numeric:
+            return self.left.value - self.right.value
+
+        raise RuntimeError('%s - %s is not supported.' % (
+            self.left.type(),
+            self.right.type()
+        ))
+
 
 class MultiplyExpression(BinaryExpression):
     def __init__(self, left, right):
