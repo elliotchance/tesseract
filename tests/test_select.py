91171b2c04f86f751c262511907c5b7a854961fd
Elliot Chance <elliotchance@gmail.com>
2015-02-28 18:59:42 +1100

    Refactor test_parser_select

--- tests/test_select.py
+++ tests/test_select.py
@@ -23,34 +23,6 @@ class TestSelect(ParserTestCase):
         self.assertTrue(result.success)
         self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
 
-    def test_where_or_parse(self):
-        sql = 'SELECT * FROM table1 WHERE foo > 123 AND foo < 125'
-        result = parser.parse(sql)
-        self.assertEquals(str(result.statement), sql)
-
-    def test_where_or_execute(self):
-        server = Server()
-        self.load_table1(server)
-
-        sql = 'SELECT * FROM table1 WHERE foo > 123 AND foo < 125'
-        result = server.execute(sql)
-        self.assertTrue(result.success)
-        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
-
-    def test_where_greater_than_equal_parse(self):
-        sql = 'SELECT * FROM table1 WHERE foo >= 124'
-        result = parser.parse(sql)
-        self.assertEquals(str(result.statement), sql)
-
-    def test_where_greater_than_equal_execute(self):
-        server = Server()
-        self.load_table1(server)
-
-        sql = 'SELECT * FROM table1 WHERE foo >= 124'
-        result = server.execute(sql)
-        self.assertTrue(result.success)
-        self.assertEqual(sorted(result.data), sorted([{'foo': 124}, {'foo': 125}]))
-
     def test_simple_parse(self):
         sql = 'SELECT * FROM table1'
         result = parser.parse(sql)
@@ -64,20 +36,6 @@ class TestSelect(ParserTestCase):
         result = server.execute(sql)
         self.assertTrue(result.success)
         self.assertEqual(sorted(result.data), sorted([{'foo': 123}, {'foo': 124}, {'foo': 125}]))
-
-    def test_where_equal_reverse_parse(self):
-        sql = 'SELECT * FROM table1 WHERE 124 = foo'
-        result = parser.parse(sql)
-        self.assertEquals(str(result.statement), sql)
-
-    def test_where_equal_reverse_execute(self):
-        server = Server()
-        self.load_table1(server)
-
-        sql = 'SELECT * FROM table1 WHERE 124 = foo'
-        result = server.execute(sql)
-        self.assertTrue(result.success)
-        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
 
     def test_where_greater_than_parse(self):
         sql = 'SELECT * FROM table1 WHERE foo > 124'
@@ -107,19 +65,11 @@ class TestSelect(ParserTestCase):
         self.assertTrue(result.success)
         self.assertEqual(sorted(result.data), sorted([{'foo': 123}, {'foo': 124}]))
 
-    def test_where_add_parse(self):
-        sql = 'SELECT * FROM table1 WHERE foo = 100 + 24'
-        result = parser.parse(sql)
-        self.assertEquals(str(result.statement), sql)
+    def test_fail_3(self):
+        self.assertFailure('SELECT * FROM', 'Expected table name after FROM.')
 
-    def test_where_add_execute(self):
-        server = Server()
-        self.load_table1(server)
-
-        sql = 'SELECT * FROM table1 WHERE foo = 100 + 24'
-        result = server.execute(sql)
-        self.assertTrue(result.success)
-        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
+    def test_fail_2(self):
+        self.assertFailure('SELECT *', 'Missing FROM clause.')
 
     def test_fail_1(self):
         self.assertFailure('SELECT', 'Expected expression after SELECT.')
@@ -152,20 +102,6 @@ class TestSelect(ParserTestCase):
         self.assertTrue(result.success)
         self.assertEqual(sorted(result.data), sorted([{'foo': 123}]))
 
-    def test_where_divide_parse(self):
-        sql = 'SELECT * FROM table1 WHERE foo = 248 / 2'
-        result = parser.parse(sql)
-        self.assertEquals(str(result.statement), sql)
-
-    def test_where_divide_execute(self):
-        server = Server()
-        self.load_table1(server)
-
-        sql = 'SELECT * FROM table1 WHERE foo = 248 / 2'
-        result = server.execute(sql)
-        self.assertTrue(result.success)
-        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
-
     def test_where_not_equal_2_parse(self):
         sql = 'SELECT * FROM table1 WHERE foo != 124'
         result = parser.parse(sql)
@@ -181,20 +117,6 @@ class TestSelect(ParserTestCase):
         self.assertTrue(result.success)
         self.assertEqual(sorted(result.data), sorted([{'foo': 123}, {'foo': 125}]))
 
-    def test_where_subtract_parse(self):
-        sql = 'SELECT * FROM table1 WHERE foo = 150 - 26'
-        result = parser.parse(sql)
-        self.assertEquals(str(result.statement), sql)
-
-    def test_where_subtract_execute(self):
-        server = Server()
-        self.load_table1(server)
-
-        sql = 'SELECT * FROM table1 WHERE foo = 150 - 26'
-        result = server.execute(sql)
-        self.assertTrue(result.success)
-        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
-
     def test_where_not_equal_1_parse(self):
         sql = 'SELECT * FROM table1 WHERE foo <> 124'
         result = parser.parse(sql)
@@ -208,4 +130,88 @@ class TestSelect(ParserTestCase):
         result = server.execute(sql)
         self.assertTrue(result.success)
         self.assertEqual(sorted(result.data), sorted([{'foo': 123}, {'foo': 125}]))
+
+    def test_where_or_parse(self):
+        sql = 'SELECT * FROM table1 WHERE foo > 123 AND foo < 125'
+        result = parser.parse(sql)
+        self.assertEquals(str(result.statement), sql)
+
+    def test_where_or_execute(self):
+        server = Server()
+        self.load_table1(server)
+
+        sql = 'SELECT * FROM table1 WHERE foo > 123 AND foo < 125'
+        result = server.execute(sql)
+        self.assertTrue(result.success)
+        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
+
+    def test_where_greater_than_equal_parse(self):
+        sql = 'SELECT * FROM table1 WHERE foo >= 124'
+        result = parser.parse(sql)
+        self.assertEquals(str(result.statement), sql)
+
+    def test_where_greater_than_equal_execute(self):
+        server = Server()
+        self.load_table1(server)
+
+        sql = 'SELECT * FROM table1 WHERE foo >= 124'
+        result = server.execute(sql)
+        self.assertTrue(result.success)
+        self.assertEqual(sorted(result.data), sorted([{'foo': 124}, {'foo': 125}]))
+
+    def test_where_equal_reverse_parse(self):
+        sql = 'SELECT * FROM table1 WHERE 124 = foo'
+        result = parser.parse(sql)
+        self.assertEquals(str(result.statement), sql)
+
+    def test_where_equal_reverse_execute(self):
+        server = Server()
+        self.load_table1(server)
+
+        sql = 'SELECT * FROM table1 WHERE 124 = foo'
+        result = server.execute(sql)
+        self.assertTrue(result.success)
+        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
+
+    def test_where_add_parse(self):
+        sql = 'SELECT * FROM table1 WHERE foo = 100 + 24'
+        result = parser.parse(sql)
+        self.assertEquals(str(result.statement), sql)
+
+    def test_where_add_execute(self):
+        server = Server()
+        self.load_table1(server)
+
+        sql = 'SELECT * FROM table1 WHERE foo = 100 + 24'
+        result = server.execute(sql)
+        self.assertTrue(result.success)
+        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
+
+    def test_where_divide_parse(self):
+        sql = 'SELECT * FROM table1 WHERE foo = 248 / 2'
+        result = parser.parse(sql)
+        self.assertEquals(str(result.statement), sql)
+
+    def test_where_divide_execute(self):
+        server = Server()
+        self.load_table1(server)
+
+        sql = 'SELECT * FROM table1 WHERE foo = 248 / 2'
+        result = server.execute(sql)
+        self.assertTrue(result.success)
+        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
+
+    def test_where_subtract_parse(self):
+        sql = 'SELECT * FROM table1 WHERE foo = 150 - 26'
+        result = parser.parse(sql)
+        self.assertEquals(str(result.statement), sql)
+
+    def test_where_subtract_execute(self):
+        server = Server()
+        self.load_table1(server)
+
+        sql = 'SELECT * FROM table1 WHERE foo = 150 - 26'
+        result = server.execute(sql)
+        self.assertTrue(result.success)
+        self.assertEqual(sorted(result.data), sorted([{'foo': 124}]))
 
