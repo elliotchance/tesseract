5399ee8e9eec78b7bb2a00c8cb47cac35710a1ee
Elliot Chance <elliotchance@gmail.com>
2015-03-04 18:54:44 +1100

    colums_are_case_sensitive

--- tesseract/sql/test_parser_values.py
+++
@@ -1,8 +0,0 @@
-from tesseract.sql.parser_test_case import ParserTestCase
-
-class TestParserValues(ParserTestCase):
-    def test_identifier(self):
-        self.assertSQL('truthy', 'truthy')
-
-    def test_identifier_case(self):
-        self.assertSQL('Truthy', 'Truthy')
