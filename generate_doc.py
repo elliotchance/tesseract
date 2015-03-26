87069d0e0ebfd47eb4161489fb8d3b09691f5ca2
Elliot Chance <elliot@kounta.com>
2015-03-26 16:12:20 +1100

    Docs do not need to be prefixed with number

--- generate_doc.py
+++ generate_doc.py
@@ -1,26 +1,39 @@
 import markdown
 import os
 import StringIO
+import re
+
+def get_title(string, default_number):
+    # The title starts with the index number.
+    if re.match('^\d+_', string):
+        return string.replace('_', ' ').split(' ', 1)
+
+    # We must use the default number.
+    return [str(default_number), string.replace('_', ' ')];
 
 def index_folder(path, prefix, indent):
     out = ''
-    for file in os.listdir(path):
+    number = 1
+    for file in sorted(os.listdir(path)):
         file_path = '%s/%s' % (path, file)
         if os.path.isdir(file_path):
-            parts = file.replace('_', ' ').split(' ', 2)
-            title = prefix + '. '.join(parts)
+            parts = get_title(file, number)
+            title = '. '.join(parts)
             out += '%s%s%s<br />\n' % ('&nbsp;' * indent, prefix, title)
-            out += index_folder(file_path, '%s.' % parts[0], indent + 4)
+            out += index_folder(file_path, prefix + '%s.' % parts[0], indent + 4)
+            number += 1
         elif file.endswith('.md'):
-            title = '. '.join(file[:-3].replace('_', ' ').split(' ', 2))
+            parts = get_title(file[:-3], number)
+            title = '. '.join(parts)
             out += '%s<a href="%s.html">%s%s</a><br />\n' % ('&nbsp;' * indent, file[:-3], prefix, title)
+            number += 1
     return out
 
-def process_folder(path):
+def process_folder(path, depth):
     for file in os.listdir(path):
         file_path = '%s/%s' % (path, file)
         if os.path.isdir(file_path):
-            process_folder(file_path)
+            process_folder(file_path, depth + 1)
         elif file.endswith('.md'):
             print(file_path)
             output = StringIO.StringIO()
@@ -34,19 +47,16 @@ def process_folder(path):
                 ]
             )
             with open('%shtml' % file_path[:-2], 'w') as html:
+                style_css = ("../" * depth) + "style.css"
                 html.write("""
-<link rel="stylesheet" type="text/css" href="../style.css">
+<link rel="stylesheet" type="text/css" href="%s">
 <table width="100%%">
   <tr>
     <td width="200" valign="top">%s</td>
     <td valign="top"><div class="markdown-body">%s</div></td>
   </tr>
 </table>
-                """ % (index, output.getvalue()))
-
-
-
+                """ % (style_css, index, output.getvalue()))
 
 index = index_folder('doc', '', 0)
-
-process_folder('doc')
+process_folder('doc', 0)
