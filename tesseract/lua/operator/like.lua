a4313c8b2c100928c1ca08ee5270971cd4fd99ae
Elliot Chance <elliotchance@gmail.com>
2015-03-28 14:53:15 +1100

    value_is_null regex_is_null both_null

--- tesseract/lua/operator/like.lua
+++ tesseract/lua/operator/like.lua
@@ -1,4 +1,9 @@
 local function operator_like(value, regex)
+    -- If either value is null then the result is always null.
+    if value == cjson.null or regex == cjson.null then
+        return cjson.null
+    end
+
     regex = '^' .. regex:gsub('%%', '.*') .. '$'
     return value:find(regex) ~= nil
 end
