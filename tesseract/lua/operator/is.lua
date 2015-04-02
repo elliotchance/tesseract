1b78b909dd38cba81fc874ca0cdf640558cccdab
Elliot Chance <elliot@kounta.com>
2015-04-02 16:45:35 +1100

    More cases

--- tesseract/lua/operator/is.lua
+++ tesseract/lua/operator/is.lua
@@ -2,8 +2,17 @@ local function operator_is(left, right)
     if right == 'true' then
         return left == true;
     end
+    if right == 'false' then
+        return left == false;
+    end
+    if right == 'boolean' then
+        return type(left) == 'boolean';
+    end
     if right == 'number' then
         return type(left) == 'number';
+    end
+    if right == 'string' then
+        return type(left) == 'string';
     end
     return left == cjson.null or left == nil
 end
