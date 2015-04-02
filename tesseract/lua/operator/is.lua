4330bff2af815ef3d71c9104947f0bfa206ebe76
Elliot Chance <elliot@kounta.com>
2015-04-02 16:26:21 +1100

    IS TRUE

--- tesseract/lua/operator/is.lua
+++ tesseract/lua/operator/is.lua
@@ -1,4 +1,7 @@
 local function operator_is(left, right)
+    if right == 'true' then
+        return left == true;
+    end
     if right == 'number' then
         return type(left) == 'number';
     end
