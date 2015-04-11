e906ddb11041eb3e0a87e2953b17cecb4c591f6c
Elliot Chance <elliotchance@gmail.com>
2015-04-11 12:53:14 +1000

    drop_notification_that_doesnt_exist

--- tesseract/engine/statements/drop_notification.py
+++ tesseract/engine/statements/drop_notification.py
@@ -4,5 +4,13 @@ from tesseract.server_result import ServerResult
 class DropNotification:
     def execute(self, result, notifications):
         notification_name = str(result.statement.notification_name)
+
+        # A notification must exist.
+        if notification_name not in notifications:
+            error = "No such notification '%s'." % notification_name
+            return ServerResult(False, error=error)
+
+        # Remove the notification.
         notifications.pop(notification_name, None)
+
         return ServerResult(True)
