54d314a26a2e7cbcab249c598df0012f7b747eb4
Elliot Chance <elliot@kounta.com>
2015-04-17 14:33:07 +1000

    update_multiple_columns

--- tesseract/engine/stage/update.py
+++ tesseract/engine/stage/update.py
@@ -4,7 +4,7 @@ from tesseract.engine.stage.where import WhereStage
 class UpdateStage(WhereStage):
     def __init__(self, input_page, offset, columns, where):
         WhereStage.__init__(self, input_page, offset, where)
-        
+
         self.columns = columns
 
     def action_on_match(self):
@@ -16,7 +16,7 @@ class UpdateStage(WhereStage):
             )
 
         lua.extend((
-            "data = cjson.encode(row)"
+            "data = cjson.encode(row)",
             "redis.call('HSET', '%s', rowid, data)" % self.input_page,
         ))
 
